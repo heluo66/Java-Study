be86fd9308b20db97e7e7af629f53323e5426e02
firstGit <firstGit@163.com>
2023-03-12 19:44:46 +0800

    git 2 commit for merge

--- src/main/GitTest.java
+++ src/main/GitTest.java
@@ -2,7 +2,7 @@ package main;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.print("git commit 1 for merge");
+        System.out.print("git commit 1 for merge 2 for  merge");
         System.out.print("git commit 2");
         System.out.print("git commit 3");
     }
