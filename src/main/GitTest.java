3004fcc6bb90bece0745e6899bbf648582abf7ab
heluo66 <127269431+heluo66@users.noreply.github.com>
2023-03-12 21:31:14 +0800

    test
    test

--- src/main/GitTest.java
+++ src/main/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.print("git commit 1 for merge 2 for  merge");
         System.out.print("git commit 2 for hot-fix");
         System.out.print("git commit 3");
+        System.out.print("for pull test");
     }
 }
